5d325c6a2b9e8654f89454f97fe30604ec663b70
Bonjemus <59416055+Bonjemus@users.noreply.github.com>
2021-02-20 11:19:36 +0100

    Add files via upload

---
+++ RespawnTimer/Config.cs
@@ -0,0 +1,11 @@
+﻿using Synapse.Config;
+using System.ComponentModel;
+
+namespace RespawnTimer
+{
+    public class PluginConfig : AbstractConfigSection
+    {
+        [Description("Is it on ? ")]
+        public bool activated = true;
+    }
+}
---
+++ RespawnTimer/PluginClass.cs
@@ -0,0 +1,25 @@
+﻿using Synapse.Api.Plugin;
+
+namespace RespawnTimer
+{
+    [PluginInformation(
+        Name = "Respawn Timer",
+        Author = "Bonjemus",
+        Description = "Plugin which allows spectators to see how much time they have to wait until the next respawn.",
+        LoadPriority = 1,
+        SynapseMajor = SynapseController.SynapseMajor,
+        SynapseMinor = SynapseController.SynapseMinor,
+        SynapsePatch = SynapseController.SynapsePatch,
+        Version = "1.0.0"
+        )]
+    public class PluginClass : AbstractPlugin
+    {
+        [Config(section = "Respawn Timer")]
+        public static PluginConfig Config;
+
+        public override void Load()
+        {
+            new Handler();
+        }
+    }
+}
